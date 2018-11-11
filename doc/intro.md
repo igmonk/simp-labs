ddec291c2644dcd3feea5696a14bc56f3d23331d
Igor Moiseyenko <imoiseyenko93@gmail.com>
2018-11-11 23:28:57 +0100

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to simp-labs
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/simp_labs/core_test.clj
@@ -0,0 +1,7 @@
+(ns simp-labs.core-test
+  (:require [clojure.test :refer :all]
+            [simp-labs.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
