3ae864d0af1f61cfbea4306407794597c64554a7
Igor Moiseyenko <imoiseyenko93@gmail.com>
2018-11-17 18:00:25 +0100

    Gate models.

---
+++ src/simp_labs/common/gate.clj
@@ -0,0 +1,15 @@
+(ns simp-labs.common.gate)
+
+(defrecord GateIn [id])
+
+(defrecord GateNot [id in])
+
+(defrecord Gate2Or [id in1 in2])
+
+(defrecord Gate2And [id in1 in2])
+
+(defrecord Gate2Nor [id in1 in2])
+
+(defrecord Gate2Nand [id in1 in2])
+
+(defrecord Gate3Nand [id in1 in2 in3])
