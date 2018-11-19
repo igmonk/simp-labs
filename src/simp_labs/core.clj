d2ad0e8fe743bff6eba8e3f6f538ea7b3725fe62
Igor Moiseyenko <imoiseyenko93@gmail.com>
2018-11-20 00:59:09 +0100

    Run lab2.

--- src/simp_labs/core.clj
+++ src/simp_labs/core.clj
@@ -1,8 +1,10 @@
 (ns simp-labs.core
   (:gen-class)
-  (:require [simp-labs.lab1.main :as lab1]))
+  (:require [simp-labs.lab1.main :as lab1]
+            [simp-labs.lab2.main :as lab2]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (lab1/define-test-sets))
+  (lab1/define-test-sets)
+  (lab2/define-test-cubes))
