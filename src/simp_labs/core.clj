21dd705d3aa35e34df7bdad6e09d77a92e095a56
Igor Moiseyenko <imoiseyenko93@gmail.com>
2018-11-25 15:11:06 +0100

    Run lab4.

--- src/simp_labs/core.clj
+++ src/simp_labs/core.clj
@@ -2,11 +2,13 @@
   (:gen-class)
   (:require [simp-labs.lab1.main :as lab1]
             [simp-labs.lab2.main :as lab2]
-            [simp-labs.lab3.main :as lab3]))
+            [simp-labs.lab3.main :as lab3]
+            [simp-labs.lab4.main :as lab4]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (lab1/define-test-sets)
   (lab2/define-test-cubes)
-  (lab3/lfsr-test-sets))
+  (lab3/lfsr-test-sets)
+  (lab4/run-ram-tests))
