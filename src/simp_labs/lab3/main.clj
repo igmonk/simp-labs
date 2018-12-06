6a338e4d3944a020577504401ac9eefe40c9d930
Igor Moiseyenko <imoiseyenko93@gmail.com>
2018-12-07 01:22:31 +0300

    LFSR update: apply LFSR to every test set (gate/fault).

--- src/simp_labs/lab3/main.clj
+++ src/simp_labs/lab3/main.clj
@@ -3,12 +3,8 @@
             [simp-labs.lab1.testable-gate :as lab1-gate]
             [simp-labs.lab3.lfsr :refer :all]))
 
-(defn unique-lab1-test-sets [f1 f2]
-  (->> (flatten (vals f1))
-       (concat (flatten (vals f2)))
-       distinct
-       (map vals)
-       set))
+(defn test-set-map->test-set [test-set-map]
+  (set (map vals (:test-set test-set-map))))
 
 (defn n-seq [n coll]
   (lazy-seq (cons (take n coll)
@@ -16,51 +12,52 @@
 
 (def not-contains? (complement contains?))
 
+(defn not-contains-x? [x]
+  (fn [s] (not-contains? s x)))
+
 (defn lfsr-cycles [test-sets lfsr-states total]
-  (if-not (or (empty? test-sets)
-              (empty? lfsr-states))
-    (let [count-to-skip (count (take-while #(not-contains? test-sets %) lfsr-states))
-          consumed (inc count-to-skip)]
-      (recur (disj test-sets (first (drop count-to-skip lfsr-states)))
-             (drop consumed lfsr-states)
-             (+ total consumed)))
-    total))
+  (if (or (empty? test-sets) (empty? lfsr-states))
+    total
+    (let [not-contains-state? (not-contains-x? (first lfsr-states))
+          filtered-test-sets (filter not-contains-state? test-sets)
+          was-found? (not= (count test-sets) (count filtered-test-sets))]
+      (recur filtered-test-sets
+             (if was-found? lfsr-states (rest lfsr-states))
+             (if was-found? total (inc total))))))
 
 (defn apply-x8+x7+x6+x3+x2+x1+1 [test-sets lfsr8-init-states]
   (letfn [(reducer [result lfsr-state]
             (let [lfsr8-coll (apply lfsr-seq-8 lfsr-state)
                   lfsr-states (take (count lfsr8-init-states) lfsr8-coll)
                   lfsr-states-7 (map rest lfsr-states)
-                  cycles (lfsr-cycles test-sets lfsr-states-7 0)]
+                  cycles (lfsr-cycles test-sets lfsr-states-7 1)]
               (if (< cycles (:lfsr-cycles result))
                 {:init-state lfsr-state :lfsr-cycles cycles}
                 result)))]
     (reduce reducer {:init-state nil :lfsr-cycles Integer/MAX_VALUE} lfsr8-init-states)))
+
+(defn test-set-mapper [fault]
+  (fn [[k v]]
+    {:gate k
+     :fault fault
+     :test-set v}))
 
 (defn lfsr-test-sets []
   (let [circuit (lab1/create-circuit)
         f1-test-sets (lab1-gate/test-inputs circuit [] 0)
         f0-test-sets (lab1-gate/test-inputs circuit [] 1)
 
-        lab1-test-sets (unique-lab1-test-sets f1-test-sets f0-test-sets)
+        f1-test-maps (map (test-set-mapper 1) f1-test-sets)
+        f0-test-maps (map (test-set-mapper 0) f0-test-sets)
+        lab1-test-set-maps (concat f1-test-maps f0-test-maps)
+        lab1-test-sets-all (map test-set-map->test-set lab1-test-set-maps)
 
         lfsr-m8 8
         lfsr-m8-l (- (Math/pow 2 lfsr-m8) 1)
         lfsr8-coll (lfsr-seq-8 1 1 1 1 1 1 1 1)
         lfsr8-states (take lfsr-m8-l lfsr8-coll)
 
-        results-8 (apply-x8+x7+x6+x3+x2+x1+1 lab1-test-sets lfsr8-states)]
-
-    (prn (str "Num of unique test input sets = " (count lab1-test-sets)))
-    (prn (str "Period = " lfsr-m8-l))
-
-    (prn "test sets:")
-    (prn lab1-test-sets)
-    (newline)
-
-    (prn "x8+x7+x6+x3+x2+x+1 LFSR states:")
-    (prn lfsr8-states)
-    (newline)
+        lab1-result-8 (apply-x8+x7+x6+x3+x2+x1+1 lab1-test-sets-all lfsr8-states)]
 
     (prn "x8+x7+x6+x3+x2+x1+1 LFSR applied to lab1 test sets:")
-    (prn results-8)))
+    (prn lab1-result-8)))
